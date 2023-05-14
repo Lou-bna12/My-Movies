a06a4045e54a9857c4ad0c086a457560e76e5143
Loubna Sellam <loubnasellam49@gmail.com>
2023-05-14 02:04:06 +0200

    git commit

---
+++ My-Movies/entities/Movie.php
@@ -0,0 +1,17 @@
+<?php
+
+class Movie {
+    // Attributs
+    private init $id:
+    private string $title;
+    private string $description;
+    private string $image_url;
+    private string $release_date;
+    private init $category_id;
+    private string $director; 
+    
+
+    // Constructeur
+
+    // Méthodes
+}
